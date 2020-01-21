9e0f01afa5d462a25695f56dc6ad6e4ade429218
Oleksandr Nechaiev <punk24664@gmail.com>
2020-01-21 07:05:56 +0100

    index unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import React, { useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
